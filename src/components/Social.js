abc896f40b9853408ae3ed41461214899959ff63
Pai026 <abhirampai1999@gmail.com>
2021-01-23 10:54:57 +0530

    added twitter badge

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,7 +7,7 @@ return (
 <a href="https://Instagram.com/abhiram_pai" target="_blank"><i class="fab fa-instagram"></i></a>
 <a href="https://www.facebook.com/abhirampai99" target="_blank"><i class="fab fa-facebook-f"></i></a>
 <a href="https://www.linkedin.com/in/abhiram-r-pai-bb3288170/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-
+<a href="https://www.twitter.com/pai_abhiram" target="_blank"><i class="fab fa-twitter"></i></a>
 <a href="https://medium.com/@abhirampai1999" target="_blank"><i class="fab fa-medium-m"></i></a>
 </div>
 )
