b620e8ec82b1f398bb8c507f625dc3ae75dcf1d5
Pai026 <abhirampai1999@gmail.com>
2021-02-06 21:58:03 +0530

    fixes

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -7,7 +7,7 @@ render() {
 return (
 <div className="bg-gray-300  home">
 <img src={profilepic} className="profilepic"></img>
-<ReactTypingEffect className="typingeffect" text={['I am Abhiram R Pai','I am a Final Year B.Tech Student','At Muthoot Institute Of Science and Technology']} speed={100} eraseDelay={700}/>
+<ReactTypingEffect className="typingeffect" text={['I am Abhiram R Pai','I am a Final Year B.Tech Student','At Muthoot Institute Of Technology and Science']} speed={100} eraseDelay={700}/>
 <Social />
 </div>
 )
