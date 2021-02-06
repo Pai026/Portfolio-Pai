8b71b961cd20cc5ebf4561483abb28e060d91708
Pai026 <abhirampai1999@gmail.com>
2021-02-06 21:02:17 +0530

    responsive design for document pages

--- src/contents/Resume.js
+++ src/contents/Resume.js
@@ -25,22 +25,24 @@ export default function Resume(props) {
   const { pdf } = props;
 
   return (
-    <div className="condiv home">
-      <Document
+    <div id="ResumeContainer">
+      <Document className={"PDFDocument"}
         file={pdf}
         options={{ workerSrc: "/pdf.worker.js" }}
         onLoadSuccess={onDocumentLoadSuccess}
       >
-        <Page pageNumber={pageNumber} />
+        <Page className={"PDFPage PDFPageOne"} pageNumber={pageNumber} />
       </Document>
-      <div>
+      
         <p>
           Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
         </p>
-        <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-          Previous
+        <div className="inline-flex">
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l" type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+          Prev
         </button>
         <button
+        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
           type="button"
           disabled={pageNumber >= numPages}
           onClick={nextPage}
