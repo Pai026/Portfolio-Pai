016c21b63e633ba0b1078cf6324aaa3dfcd14919
Pai026 <abhirampai1999@gmail.com>
2021-02-07 17:07:06 +0530

    added loader

--- src/contents/Resume.js
+++ src/contents/Resume.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Document, Page } from "react-pdf";
-
+import Loader from "react-loader-spinner";
 export default function Resume(props) {
     const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
+    const [loading,setLoading] = useState(true);
+    const [pageNumber, setPageNumber] = useState(1); //setting 1 to show first page
 
     function onDocumentLoadSuccess({ numPages }) {
+        setLoading(false)
         setNumPages(numPages);
         setPageNumber(1);
     }
@@ -30,7 +32,15 @@ export default function Resume(props) {
                 className={"PDFDocument"}
                 file={pdf}
                 options={{ workerSrc: "/pdf.worker.js" }}
-                onLoadSuccess={onDocumentLoadSuccess}>
+                onLoadSuccess={onDocumentLoadSuccess}
+                loading={<Loader
+                    type="Puff"
+                    color="#00BFFF"
+                    height={100}
+                    width={100}
+                    timeout={loading} //3 secs
+                  />}
+                >
                 <Page
                     className={"PDFPage PDFPageOne"}
                     pageNumber={pageNumber}
