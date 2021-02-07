43d79193b56820d201d5e5f62056df3ecf96b30c
Pai026 <abhirampai1999@gmail.com>
2021-02-07 18:02:15 +0530

    mobile responsiveness edit

--- src/contents/Resume.js
+++ src/contents/Resume.js
@@ -31,14 +31,15 @@ export default function Resume(props) {
                 file={pdf}
                 options={{ workerSrc: "/pdf.worker.js" }}
                 onLoadSuccess={onDocumentLoadSuccess}
-                loading={<Loader
-                    type="Puff"
-                    color="#00BFFF"
-                    height={100}
-                    width={100}
-                    timeout={3000} //3 secs
-                  />}
-                >
+                loading={
+                    <Loader
+                        type="Puff"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                        timeout={10000} //3 secs
+                    />
+                }>
                 <Page
                     className={"PDFPage PDFPageOne"}
                     pageNumber={pageNumber}
